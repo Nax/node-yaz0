ca60f22a4c79d0de6a548279054921dcb241b42b
Nax <max@bacoux.com>
2024-10-28 03:23:28 +0100

    Update build.sh, fix a stack issue

--- build.sh
+++ build.sh
@@ -3,11 +3,10 @@
 mkdir -p dist/browser
 
 emcc -Ilibyaz0/include -O3 --closure 1 \
-  --memory-init-file 0 \
   -sWASM=0 \
   -sALLOW_MEMORY_GROWTH=1 \
   -sNO_FILESYSTEM=1 \
-  -sENVIRONMENT='web' \
+  -sENVIRONMENT='node' \
   -sEXPORTED_FUNCTIONS=_malloc,_free,_realloc,_emYaz0Init,_yaz0Destroy,_yaz0ModeDecompress,_yaz0ModeCompress,_yaz0Run,_yaz0Input,_yaz0Output,_yaz0OutputChunkSize,_yaz0DecompressedSize \
   libyaz0/src/libyaz0/compress.c \
   libyaz0/src/libyaz0/decompress.c \
