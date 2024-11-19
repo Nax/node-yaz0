5aaec465bb87fac1cebf95a1ec2d44553f1d9c43
Nax <max@bacoux.com>
2024-11-19 09:49:35 +0100

    Move to ES modules + WASM

--- build.sh
+++ build.sh
@@ -1,23 +1,16 @@
 #!/bin/sh
 
-mkdir -p dist/browser
-mkdir -p dist/util
-
-emcc -Ilibyaz0/include -Oz --closure 2 \
+emcc -Ilibyaz0/include -Oz --closure 1 \
   -sENVIRONMENT=web \
-  -sWASM=0 \
+  -sWASM=1 \
+  -sEXPORT_ES6=1 \
   -sMODULARIZE=1 \
-  -sMALLOC=emmalloc \
   -sFILESYSTEM=0 \
-  -sWASM_ASYNC_COMPILATION=0 \
+  -sMALLOC=emmalloc \
   -sEXPORTED_FUNCTIONS=_malloc,_free,_realloc,_emYaz0Init,_yaz0Destroy,_yaz0ModeDecompress,_yaz0ModeCompress,_yaz0Run,_yaz0Input,_yaz0Output,_yaz0OutputChunkSize,_yaz0DecompressedSize \
   libyaz0/src/libyaz0/compress.c \
   libyaz0/src/libyaz0/decompress.c \
   libyaz0/src/libyaz0/libyaz0.c \
   libyaz0/src/libyaz0/util.c \
   src/browser/yaz0_browser.c \
-  -o src/browser/libyaz0.js
-
-./node_modules/.bin/rollup --format cjs --config ./rollup.config.js ./src/browser/index.js --file dist/browser/index.js
-cp -r src/node/*.js dist/node/
-cp -r src/util/*.js dist/util/
+  -o lib/browser/libyaz0.js
